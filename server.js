b857d6a4ad7445f837821c212d1aa2a79b90db40
Seungil Park <spark263@my.bcit.ca>
2018-12-04 19:53:17 -0800

    please!!!!!!!!!

--- server.js
+++ server.js
@@ -1,20 +1,11 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
 
-// Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client1/build')));
 
-// Put all API endpoints under '/api'
-
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client1/build/index.html'));
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'client1/build', 'index.html'));
 });
 
-const port = process.env.PORT || 5000;
-app.listen(port);
-
-console.log(`Password generator listening on ${port}`);
+app.listen(9000);
